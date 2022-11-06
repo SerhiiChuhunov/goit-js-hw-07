3e045cc25b51deb071a599bed9867dc8136084a0
Serhii Chuhunov <patricknamaste@gmail.com>
2022-11-06 08:09:50 +0200

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const makeGalleryItems = element => {
+  const { preview, original, description } = element;
+
+  return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `;
+};
+
+const gallery = document.querySelector('.gallery');
+
+const makeGallery = galleryItems.map(makeGalleryItems).join('');
+gallery.innerHTML = makeGallery;
+
+const instance = basicLightbox.create(
+  `<img class="modal-img" src= ''>`,
+  {
+    onShow: () => window.addEventListener('keydown', onEscButtonPress),
+  },
+  {
+    onClose: () => window.addEventListener('keydown', onEscButtonPress),
+  },
+);
+gallery.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return
+  }
+  instance.element().querySelector('.modal-img').src = e.target.dataset.source;
+  instance.show();
+});
+function onEscButtonPress(e) {
+  if (e.key === 'Escape') {
+    instance.close();
+    window.removeEventListener('keydown', onEscButtonPress);
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const makeGalleryItems = element => {
+  const { preview, original, description } = element;
+  return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" /> </a>`;
+};
+const gallery = document.querySelector('.gallery');
+const makeGallery = galleryItems.map(makeGalleryItems).join('');
+gallery.innerHTML = makeGallery;
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  close: true,
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: '250',
+  captionPosition: 'bottom',
+  showCounter: true,
+  doubleTapZoom: 2,
+});
